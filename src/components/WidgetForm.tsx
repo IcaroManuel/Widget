3b5fd620785bab4e63bfc8439b1167b9e59b890c
IcaroManuel <icaroomanuel@gmail.com>
2023-02-02 17:42:57 -0300

    changes: insert CloseButton in WidgetForm

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,14 +1,29 @@
 import { CloseButton } from "./CloseButton";
 
+const feedbackTypes = {
+  Bug: {
+    title: 'Problema'
+  },
+  IDEA: {
+    title: 'Ideia'
+
+  },
+  OTHER: {
+    title: 'Outro'
+
+  }
+}
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
-        <span className='text-xl leading-6'>
-          Deixe seu Feedback
-        </span>
+        <span className='text-xl leading-6'>Deixe seu Feedback</span>
         <CloseButton />
       </header>
+
+      <div className=' flex py-8 gap-2 w-full'>
+      </div>
 
       <footer className='text-xs text-neutral-400'>
         Feito por <a className='underline underline-offser-2' href="https://github.com/IcaroManuel">Icaro</a>
